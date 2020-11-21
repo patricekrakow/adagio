bf464667257b03051a09d860e7a8c949a578ce2d
Patrice Krakow <patrice.krakow@gmail.com>
2020-11-21 17:23:03 +0100

    Update 2020-11-21

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# adagio
-A reference implementation of a service exposing an API in Node.js
+# Adagio
 
-```
+Adagio is a reference Node.js implementation of a service exposing an API.
+
+## Setup
+
+1\. Install dependencies:
+
+```bash
 $ npm install
 ```
-```
+
+2\. Start the service:
+
+```bash
 $ npm start
 ```
+
+3\. You can browse the API at <http://localhost:3000>
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 'use strict';
 const express = require('express');
-const PORT = 8080
+const bodyParser = require('body-parser');
+
+const PORT = 3000
 const HOST = '0.0.0.0';
-const SERVICE_NAME = "service1";
+const SERVICE_NAME = "adagio";
 const SERVICE_VERSION = "1.0.0";
+
 const app = express();
-app.get('/resource-a', (req, res) => {
-  res.send({
-    message: "Hello from resource-a",
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "Hello from Adagio ;-)",
     internalInfo: {
       serviceName: SERVICE_NAME,
       version: SERVICE_VERSION,
@@ -15,5 +23,7 @@ app.get('/resource-a', (req, res) => {
     }
   });
 });
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is litening on http://${HOST}:${PORT}`);
+});
